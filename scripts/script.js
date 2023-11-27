72a741317df242379654363123fbaf5e358712f7
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-11-27 23:45:19 +0200

    Revert "tranquilo amigo"
    This reverts commit 6154da60aed91518cea4fcf94832b8cdfd5ad68e.

--- scripts/script.js
+++ scripts/script.js
@@ -51,7 +51,6 @@ button.addEventListener("click", function () {
   // Event listener for checkbox change
   checkbox.addEventListener("change", function () {
     label.classList.toggle("checked", checkbox.checked);
-    saveTasks();
   });
 
   // Create a label for the task
